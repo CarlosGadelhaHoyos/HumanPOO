33127f2d7a53e928e7a9966c7e3b75ae22701011
Carlos Hoyos <carlosgadelhahoyos@gmail.com>
2023-12-10 19:09:19 -0400

    Added getters and setters

--- Human_POO/src/Human.java
+++ Human_POO/src/Human.java
@@ -1,5 +1,5 @@
 public class Human {
-    
+
     private String paleColor;
     private float height;
     private float weight;
@@ -9,7 +9,7 @@ public class Human {
     public void speak(){
 
     }
-    
+
     public void walk(){
 
     }
@@ -24,5 +24,76 @@ public class Human {
 
     public void doExercises(){
 
+    }
+
+    //Getters and Setters
+    /**
+     * @return String return the paleColor
+     */
+    public String getPaleColor() {
+        return paleColor;
+    }
+
+    /**
+     * @param paleColor the paleColor to set
+     */
+    public void setPaleColor(String paleColor) {
+        this.paleColor = paleColor;
+    }
+
+    /**
+     * @return float return the height
+     */
+    public float getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    /**
+     * @return float return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return boolean return the rightHanded
+     */
+    public boolean isRightHanded() {
+        return rightHanded;
+    }
+
+    /**
+     * @param rightHanded the rightHanded to set
+     */
+    public void setRightHanded(boolean rightHanded) {
+        this.rightHanded = rightHanded;
+    }
+
+    /**
+     * @return int return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
     }
 }
