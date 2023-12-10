9161fcc8120df99bf9d5dab0fb680df5cf2e5888
Carlos Hoyos <carlosgadelhahoyos@gmail.com>
2023-12-10 18:49:28 -0400

    Human class 1.0
    Added only key attributes and methods (only declared)

---
+++ Human_POO/src/Human.java
@@ -0,0 +1,28 @@
+public class Human {
+    
+    private String paleColor;
+    private float height;
+    private float weight;
+    private boolean rightHanded;
+    private int age;
+
+    public void speak(){
+
+    }
+    
+    public void walk(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void doExercises(){
+
+    }
+}
